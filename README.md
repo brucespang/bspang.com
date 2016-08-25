684bcb124cba8229282e1514ef1ab9e2110f1058
Bruce Spang <bruce@brucespang.com>
2016-08-25 11:43:52 -0400

    add an index

---
+++ README.md
@@ -0,0 +1,5 @@
+## To update
+
+1. Add the file
+1. Run `./generate_index.sh`
+1. Commit and push to github
---
+++ generate_index.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -e
+
+cat << EOF > index.html
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html>
+ <head>
+  <title>Index of /</title>
+ </head>
+ <body>
+<h1>Index of /</h1>
+  <table>
+   <tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th>Name</th></tr>
+   <tr><th colspan="5"><hr></th></tr>
+EOF
+
+for file in `ls`; do
+    cat << EOF >> index.html
+<tr><td valign="top"><img src="/icons/image2.gif" alt="[IMG]"></td><td><a href="$file">$file</a></td></tr>
+EOF
+done
+
+cat << EOF >> index.html
+</table>
+</body>
+</html>
+EOF
